f4715af7b55fd8f2da20615623948b734251eed7
mhmmtkkl <makoklu32@gmail.com>
2020-07-10 21:13:26 -0400

    Day1 TestNG

--- src/Day1/_03_Sample.java
+++ src/Day1/_03_Sample.java
@@ -1,7 +1,16 @@
 package Day1;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class _03_Sample {
 
+
+    @Test
+    public void test1(){
+
+    }
 
 
 }
