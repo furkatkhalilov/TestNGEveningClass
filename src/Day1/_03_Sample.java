2f1a2f0e7d41ff2d6aef7f75f592542f51cdc7aa
mhmmtkkl <makoklu32@gmail.com>
2020-07-10 21:21:04 -0400

    Day1 TestNG

--- src/Day1/_03_Sample.java
+++ src/Day1/_03_Sample.java
@@ -1,17 +1,27 @@
 package Day1;
 
+import Utils.BaseDriver;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-public class _03_Sample {
-
+public class _03_Sample extends BaseDriver {
 
     @Test
     public void test1(){
 
+        String u1 = driver.getCurrentUrl();
+
+        System.out.println(u1);
     }
 
+    @Test
+    public void test2(){
+
+        String u1 = driver.getCurrentUrl();
+
+        System.out.println(u1);
+    }
 
 }
 
--- src/Utils/BaseDriver.java
+++ src/Utils/BaseDriver.java
@@ -3,21 +3,30 @@ package Utils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 
-public class BaseDriver {
+public class BaseDriver  {
 
-    
+    /*
+     WebDriver driver; access modifier is a default --> accessible in the same package
+
+     We need to make driver as public or protected so we are able to access it
+     */
+
+    protected WebDriver driver;
+
     @BeforeMethod
     public void beforeMethod(){
         System.setProperty("webdriver.chrome.driver", "D:\\Selenium dependency\\drivers\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("http://opencart.abstracta.us/index.php?route=common/home");
+
     }
 
     @AfterMethod
     public void afterMethod(){
-
+        driver.quit();
     }
 }
