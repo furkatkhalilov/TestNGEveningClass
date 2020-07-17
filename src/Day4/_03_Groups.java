dfa2970fdec7f154b8f8321d5a3c4daedc91d743
mhmmtkkl <makoklu32@gmail.com>
2020-07-16 20:34:22 -0400

    Day4 TestNG

--- src/Day4/_03_Groups.java
+++ src/Day4/_03_Groups.java
@@ -19,10 +19,29 @@ public class _03_Groups {
         System.out.println("Test3 for Smoke");
     }
 
-//    We are able to add multiple parameter in the test
-    @Test(groups = "SmokeTest" , priority = 1)
+    @Test(groups = "SmokeTest" )
     public void Test4(){
         System.out.println("Test4 for Smoke");
     }
-    
+
+//    We are able to add multiple parameter in the test
+    @Test(groups = "SmokeTest", enabled = false )
+    public void Test5(){
+        System.out.println("Test5 for Smoke");
+    }
+
+    @Test
+    public void Test6(){
+        System.out.println("Test 6 for no group");
+    }
+
+    /*
+        Please run the Day2.ValidateMenu
+            add it in the regression test
+
+            Day2.subscribe in the smoke test
+            Day2.EditAccount in the smoke test
+     */
+
+
 }
